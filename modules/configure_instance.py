fadc93ff57f9d1e588d74814f15a453f4e9e5633
Kieran Wood <kieranw098@gmail.com>
2019-01-02 01:08:45 -0700

    Removed console-menu code

--- modules/configure_instance.py
+++ modules/configure_instance.py
@@ -3,7 +3,7 @@ This file contains all the functions and menu used
 when calling the toolbox via the configure command
 """
 from modules.script_utilities import \
-    save_config_to_file, select_directory, get_ip
+    save_config_to_file, select_directory
 
 import json
 import os
@@ -40,44 +40,10 @@ def import_config():
         dir = select_directory()
         # TODO Save JSON object
 
-def configuration_menu(CONFIG_PATH = "{}/config.json".format(str(os.getcwd()))):
+def configuration_menu():
     """ 
     The main configuration menu
     """
-    
-    # Prettying up the window with some formatting
-    menu_format = MenuFormatBuilder().set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_OUTER_LIGHT_INNER_BORDER)\
-        .set_prompt("SELECT>") \
-        .set_title_align('center') \
-        .set_subtitle_align('center') \
-        .set_left_margin(4) \
-        .set_right_margin(4) \
-        .show_header_bottom_border(True)
 
-    # Creating the initial Menu
-    menu = ConsoleMenu("Configure TicketFresh",
-        epilogue_text="Select an Option from the choices above",
-        prologue_text="The Currently Defined Config path is {}".format(CONFIG_PATH),
-        formatter=menu_format)
-
-    # Option lets you export a default conf to the config path
-    exp_default_conf = FunctionItem(
-        "Export Default config to main directory", save_config_to_file,
-        [vars(DEFAULT_CONFIG), CONFIG_PATH])
-
-    # Option lets you change the config path
-    chng_conf_path = FunctionItem(
-        "Change current configuration path", select_directory,
-        kwargs={"gui":False}
-        )
-
-    # A CommandItem runs a console command
-    create_configuration_directory = CommandItem("Creae a directory called configurations",  "mkdir configurations")
-
-    # Adding items to the menu
-    menu.append_item(exp_default_conf)
-    menu.append_item(chng_conf_path)
-    menu.append_item(create_configuration_directory)
-
-    # Finally, we call show to show the menu and allow the user to interact
-    menu.show()
+    # TODO: Recreate without the console menu module
+    CONFIG_PATH = str(os.getcwd())
